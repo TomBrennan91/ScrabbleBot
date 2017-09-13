c77c0bdac87058590e5002f48e7d18522bd58b5a
Tom Brennan <31164717+TomBrennan91@users.noreply.github.com>
2017-09-13 18:24:55 +0100

    Added Player object

--- src/LetterRack.java
+++ src/LetterRack.java
@@ -3,10 +3,11 @@ import java.util.ArrayList;
 public class LetterRack implements Constants{
 	ArrayList<Tile> tiles;
 	
-	
 	public void refill(){
 		while (tiles.size() < TILES_IN_RACK){
 			tiles.add(TileBag.getInstance().takeOutTile());
 		}
 	}
+	
+	
 }
---
+++ src/Player.java
@@ -0,0 +1,6 @@
+
+public class Player {
+	String name;
+	boolean isAI;
+	
+}
